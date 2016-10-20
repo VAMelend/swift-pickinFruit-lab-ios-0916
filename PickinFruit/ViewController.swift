2f009f4c559a5ce0ff7087d250053a4892c7f131
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-10-20 14:48:09 -0400

    done

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var fruitPicker: UIPickerView!
+    
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
@@ -19,15 +22,58 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        fruitPicker.dataSource = self
+        fruitPicker.delegate = self
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        resultLabel.text = fruitsArray[row]
+    }
+    
+    
+    
+    
+    @IBAction func spinButtonTapped(_ sender: UIButton) {
+        let row1 = arc4random_uniform(UInt32(fruitsArray.count))
+        let row2 = arc4random_uniform(UInt32(fruitsArray.count))
+        let row3 = arc4random_uniform(UInt32(fruitsArray.count))
+        let row4 = arc4random_uniform(UInt32(fruitsArray.count))
+        
+        
+        fruitPicker.selectRow(Int(row1), inComponent: 0, animated: true)
+        fruitPicker.selectRow(Int(row2), inComponent: 1, animated: true)
+        fruitPicker.selectRow(Int(row3), inComponent: 2, animated: true)
+        fruitPicker.selectRow(Int(row4), inComponent: 3, animated: true)
+        
+        let index1 = fruitPicker.selectedRow(inComponent: 0)
+        let index2 = fruitPicker.selectedRow(inComponent: 1)
+        let index3 = fruitPicker.selectedRow(inComponent: 2)
+        let index4 = fruitPicker.selectedRow(inComponent: 3)
+        
+        if (index1 == index2) && (index2 == index3) && (index3 == index4) {
+            resultLabel.text = "WINNER!"
+        }
+        else { resultLabel.text = "TRY AGAIN"
+        }
         
     }
     
-    @IBAction func spinButtonTapped(_ sender: UIButton) {
-        
-        
-        
-    }
-
+    
+    
 }
 
 // MARK: Set Up
